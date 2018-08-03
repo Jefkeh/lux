b6fd7f3014fc57c49d37e91c7373dc6a16561b29
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2018-08-03 18:13:21 +0200

    windows: update crosscompiler script
    set default arch to 64-bits, ./build-wins.sh x86 to build 32-bit ones
    
    also, dont alter ubuntu distribution and packages, left as comments
    as some of them may be required the first time.

--- depends/build-wins.sh
+++ depends/build-wins.sh
@@ -4,9 +4,9 @@
 #-###############################################-#
 
 # Set platform variables
-PLATFORM="i686-w64-mingw32"
-if [ "$1" = "x64" ]; then
-    PLATFORM="x86_64-w64-mingw32"
+PLATFORM="x86_64-w64-mingw32"
+if [ "$1" = "x86" ]; then
+   PLATFORM="i686-w64-mingw32"
 fi
 
 CC="/usr/bin/$PLATFORM-gcc-posix"
@@ -16,14 +16,14 @@ INSTALL_DIR="$OLD_PATH/$PLATFORM"
 LIB_DIR="$INSTALL_DIR/lib"
 INCLUDE_DIR="$INSTALL_DIR/include"
 
-sudo apt install software-properties-common
-sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu zesty universe"
-sudo apt update
-sudo apt upgrade
+#sudo apt install software-properties-common
+#sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu zesty universe"
+#sudo apt update
+#sudo apt upgrade
 
 # Install development tools if needed
 if [ ! -f $CC ]; then
-   sudo apt-get install build-essential libtool autotools-dev automake cmake pkg-config bsdmainutils curl g++-mingw-w64-x86-64 mingw-w64-x86-64-dev g++-mingw-w64-i686 mingw-w64-i686-dev -y
+   sudo apt-get install build-essential libtool autotools-dev automake cmake pkg-config bsdmainutils curl g++-mingw-w64-x86-64 mingw-w64-x86-64-dev g++-mingw-w64-i686 mingw-w64-i686-dev openjdk-8-jdk -y
 fi
 
 #Double check whether we have the expected toolchain
@@ -42,16 +42,9 @@ if [ -z "$CC" ] || [ -z "$CXX" ]; then
     exit
 fi
 
-if [ -z "$(echo $JAVA_HOME)" ]; then
-   echo "warning: JAVA_HOME env var is not set!"
-   #sudo apt install openjdk-8-jdk
-   sudo apt install oracle-java8-set-default
-fi
-
-if [ ! -f "$JAVA_HOME/include/jni_md.h" ]; then
-   if [ -f "$JAVA_HOME/include/linux/jni_md.h" ]; then
-      sudo ln -s "linux/jni_md.h" "$JAVA_HOME/include/jni_md.h"
-   fi
+if [ -z "$JAVA_HOME" ]; then
+   export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+   echo "JAVA_HOME env var set to $JAVA_HOME!"
 fi
 
 # Make dependencies
@@ -76,10 +69,10 @@ cp ../src/config/condition_variable.hpp "$INCLUDE_DIR/boost/thread/win32/conditi
 
 cd ..
 ./autogen.sh windows $PLATFORM $INSTALL_DIR
-./configure --prefix=$PWD/depends/$PLATFORM --host=$PLATFORM --disable-tests --disable-shared CPPFLAGS="-DMINIUPNP_STATICLIB" CFLAGS="-std=c99" LDFLAGS="-static -static-libgcc -Wl,-Bstatic -lstdc++"
+./configure --prefix=$PWD/depends/$PLATFORM --host=$PLATFORM --disable-shared --enable-reduce-exports CPPFLAGS="-DMINIUPNP_STATICLIB" CFLAGS="-std=c99 -I$JAVA_HOME/include -I$JAVA_HOME/include/linux" LDFLAGS="-static -static-libgcc -Wl,-Bstatic -lstdc++"
 
 # Build the application
-make -j$(nproc)
+make -j$(nproc) #--trace
 
 RELEASE="$OLD_PATH/Release"
 mkdir -p "$RELEASE"
